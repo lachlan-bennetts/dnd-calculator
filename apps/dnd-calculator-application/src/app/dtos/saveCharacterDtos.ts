13b25d7fdbcbdbbeb2a59549736d3486eb325f5f
lachlan-bennetts <lachlanbennetts@hotmail.com>
2024-05-09 20:09:11 +1000

    feat: finalising schema to test, will seed next commit

--- apps/dnd-calculator-application/src/app/dtos/saveCharacterDtos.ts
+++ apps/dnd-calculator-application/src/app/dtos/saveCharacterDtos.ts
@@ -1,3 +1,5 @@
+import { ClassEnum, RaceEnum } from '../utils/constants';
+
 export interface saveCharacterDto {
   playerName: string;
   characterName: string;
@@ -12,31 +14,4 @@ export interface saveCharacterDto {
   class: ClassEnum;
   subClass: string;
   userId: string;
-}
-
-enum ClassEnum {
-  BARBARIAN = "Barbarian",
-  BARD = "Bard",
-  CLERIC = "Cleric",
-  DRUID = "Druid",
-  FIGHTER = "Fighter",
-  MONK = "Monk",
-  PALADIN = "Paladin",
-  RANGER = "Ranger",
-  ROGUE = "Rogue",
-  SORCERER = "Sorcerer",
-  WARLOCK = "Warlock",
-  WIZARD = "Wizard"
-}
-
-enum RaceEnum {
-  DRAGONBORN = 'Dragonborn',
-  DWARF = 'Dwarf',
-  ELF = "Elf",
-  GNOME = "Gnome",
-  HALF_ELF = 'Half-Elf',
-  HALF_ORC = "Half-Orc",
-  HALFLING = "Halfling",
-  HUMAN = "Human",
-  TIEFLING = "Tiefling"
 }
---
+++ apps/dnd-calculator-application/src/app/utils/constants.ts
@@ -0,0 +1,78 @@
+export enum RaceEnum {
+  DRAGONBORN = 'Dragonborn',
+  DWARF = 'Dwarf',
+  ELF = "Elf",
+  GNOME = "Gnome",
+  HALF_ELF = 'Half-Elf',
+  HALF_ORC = "Half-Orc",
+  HALFLING = "Halfling",
+  HUMAN = "Human",
+  TIEFLING = "Tiefling"
+}
+
+export enum DragonbornEnum {
+  BLACK = "Black",
+  BLUE = "Blue",
+  BRASS = "Brass",
+  BRONZE = "Bronze",
+  COPPER = "Copper",
+  GOLD = "Gold",
+  GREEN = "Green",
+  RED = "Red",
+  SILVER = "Silver",
+  WHITE = "White"
+}
+
+export enum DwarfEnum {
+  HILL = "Hill",
+  MOUNTAIN = "Mountain"
+}
+
+export enum ElfEnum {
+  HIGH = "High",
+  WOOD = "Wood",
+  DARK = "Dark"
+}
+
+export enum GnomeEnum {
+  FOREST = "Forest",
+  ROCK = "Rock"
+}
+
+export enum HalfElfEnum {
+  HALF_ELF = "Half-Elf"
+}
+
+export enum HalfOrcEnum {
+  HALF_ORC = "Half-Orc"
+}
+
+export enum HalflingEnum {
+  LIGHTFOOT = "Lightfoot",
+  STOUT = "Stout"
+}
+
+export enum HumanEnum {
+  HUMAN = "Human"
+}
+
+export enum TieflingEnum {
+  Asmodeus = "Asmodeus",
+}
+
+export enum ClassEnum {
+  BARBARIAN = "Barbarian",
+  BARD = "Bard",
+  CLERIC = "Cleric",
+  DRUID = "Druid",
+  FIGHTER = "Fighter",
+  MONK = "Monk",
+  PALADIN = "Paladin",
+  RANGER = "Ranger",
+  ROGUE = "Rogue",
+  SORCERER = "Sorcerer",
+  WARLOCK = "Warlock",
+  WIZARD = "Wizard"
+}
+
+
